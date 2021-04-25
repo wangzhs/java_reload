c56dcff3858d0ccbe1835f92bf210f37b71e5347
wangzhs <915196681@qq.com>
2021-04-25 15:49:03 +0800

    first

---
+++ src/com/by/wzs/Main.java
@@ -0,0 +1,11 @@
+package com.by.wzs;
+
+/**
+ * @Author wangzhs
+ * @create 2021/4/25 15:47
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("start");
+    }
+}
